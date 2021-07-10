0da96df2e991ad77cd6fcc38938defccdfc9be90
hamad-SHE <eng_cis1@hotmail.com>
2021-07-10 16:36:49 +0300

    add struct discriptor

--- main.cpp
+++ main.cpp
@@ -195,6 +195,24 @@ void DetectSuspiciousThingsAboutProcess(PCTSTR pName)
 
     DWORD discriptAddress = ntheader.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress + beasAddressExe;
     
+
+
+    //typedef struct _IMAGE_IMPORT_DESCRIPTOR {
+    //    union {
+    //        DWORD   Characteristics;            // 0 for terminating null import descriptor
+    //        DWORD   OriginalFirstThunk;         // RVA to original unbound IAT (PIMAGE_THUNK_DATA)
+    //    } DUMMYUNIONNAME;
+    //    DWORD   TimeDateStamp;                  // 0 if not bound,
+    //                                            // -1 if bound, and real date\time stamp
+    //                                            //     in IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT (new BIND)
+    //                                            // O.W. date/time stamp of DLL bound to (Old BIND)
+
+    //    DWORD   ForwarderChain;                 // -1 if no forwarders
+    //    DWORD   Name;
+    //    DWORD   FirstThunk;                     // RVA to IAT (if bound this IAT has actual addresses)
+    //} IMAGE_IMPORT_DESCRIPTOR;
+
+
     IMAGE_IMPORT_DESCRIPTOR importDescriptor;
     
     
@@ -224,7 +242,7 @@ static int mymain() {
 
 
      getchar();
-     return 0;*/
+     return 0;
 }
 
 
