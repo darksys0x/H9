ddb38bfc1b0dd6b349247c8b5f67262fa8fc5429
hamad-SHE <eng_cis1@hotmail.com>
2021-10-20 21:56:10 +0300

    read the lines from memory heap file.

--- parserYml.cpp
+++ parserYml.cpp
@@ -1,25 +1,105 @@
-#include <stdio.h>
-#include <Windows.h>
-//1. read the file from disk to heap memory (done);
-//2. print the data from heap memory
-int fileSize = 0;
-void* heapFile = NULL;
+#include<stdio.h>
+#include<Windows.h>
+//1. get the file size from disk and allocate in memory,
 
-int main() {
-	FILE* hamadfile = fopen("C:\\Users\\DFIR\\Desktop\\H9_rewritng\\memoryRules.yml", "rb");
-	if (hamadfile) {
-		fseek(hamadfile, 0, SEEK_END);
-		fileSize = ftell(hamadfile);
-		fseek(hamadfile, 0, SEEK_SET);
-		heapFile = malloc(fileSize);
-		fread(heapFile, fileSize, 1, hamadfile);
-		fclose(hamadfile);
-		printf(" the success to read the file \n");
+
+long fileSize = 0;
+long currentOffset = 0;
+void* heapFileDate = NULL;
+
+
+
+void printLineByLineFromHeapMemory( ) {
+	char* p = (char*)heapFileDate;
+	char line[200];
+	int i = 0;
+	
+	
+	DWORD currentIndex = 0;
+	for ( ; i < fileSize; i++) {
+		line[currentIndex] = p[i];
+		if(p[i] == '\n'){
+
+			// we have a complete line now
+			
+			line[currentIndex] = '\0';
+			
+			printf("%s\n", line);
+
+			currentIndex = 0;
+			
+			
+		}
+
+		else
+		{
+			currentIndex++;
+
+		} 
+		
+
 	}
-	else {
-		printf("failed to open file\n");
+
+
+	if (fileSize == i) {
+		line[currentIndex] = '\0';
+		printf("%s\n", line);
 	}
 	
-	getchar();
-	return 0;
+
+
 }
+
+
+
+
+
+
+//
+//void printLineByLineFromHeapMemory() {
+//	char* p = (char*)heapFileDate;
+//	char line[200];
+//
+//	int lineIndex = 0;
+//	for (int i = 0; i < fileSize; i++) {
+//		//printf("%c", p[i]);
+//		if (p[i] == '\n') {
+//
+//			line[lineIndex] = '\0';
+//			printf("%s\n", line);
+//			lineIndex = 0;
+//		}
+//		else {
+//			line[lineIndex] = p[i]; // copy from file in heap to line
+//			lineIndex++;
+//		}
+//
+//	}
+//}
+
+
+int main() {
+	FILE* hamadFile = fopen("C:\\Users\\DFIR\\Desktop\\H9_rewritng\\memoryRules.yml", "rb");
+	if (hamadFile) {
+		fseek(hamadFile, 0, SEEK_END);
+		fileSize = ftell(hamadFile);
+		fseek(hamadFile, 0, SEEK_SET);
+		heapFileDate = malloc(fileSize);
+		fread(heapFileDate, fileSize, 1, hamadFile);
+		fclose(hamadFile);
+
+		
+		
+	}
+	else
+	{
+		printf("failed to read file");
+
+	}
+	printLineByLineFromHeapMemory();
+	
+}
+
+
+
+
