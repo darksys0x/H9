1b73db064bf87d0e8d25063b863915118eafbf52
hamad-SHE <eng_cis1@hotmail.com>
2022-06-05 13:03:06 +0300

    test

--- h9/main.cpp
+++ h9/main.cpp
@@ -2,12 +2,52 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 #include <processthreadsapi.h>
+#include <winternl.h>
 #pragma comment( lib, "Kernel32.lib" )
+
+
+typedef struct _PEB_LDR_DATA88
+{
+	ULONG Length;
+	UCHAR Initialized;
+	PVOID SsHandle;
+
+} PEB_LDR_DATA88, * PPEB_LDR_DATA88;
+
+
+typedef struct _PEB88 {
+	BYTE                          Reserved1[2];
+	BYTE                          BeingDebugged;
+	BYTE                          Reserved2[1];
+	PVOID                         Reserved3[2];
+	PPEB_LDR_DATA                 Ldr;
+} PEB88, * PPEB88;
+
+
+typedef NTSTATUS(__stdcall* NtQueryInformationProcess)
+(
+	HANDLE           ProcessHandle,
+	PROCESSINFOCLASS ProcessInformationClass,
+	PVOID            ProcessInformation,
+	ULONG            ProcessInformationLength,
+	PULONG           ReturnLength
+);
+
+
+DWORD getBaseAddressOfprocess(void* Dedct) {
+	HMODULE ntdllBaseAddress = GetModuleHandle(L"ntdll.dll");
+	NtQueryInformationProcess hamadProcessbaseAddress = (NtQueryInformationProcess)GetProcAddress(ntdllBaseAddress, "NtQueryInformationProcess");
+	PROCESS_BASIC_INFORMATION baseAddressh;
+
+	const int i = sizeof(PEB_LDR_DATA88);
+
+}
+
 
 
 void DetectSuspiciousFunction(DWORD processId) {
 	HANDLE hpOpenProcess = OpenProcess(0x0010 | 0x0020, FALSE, processId);
-
+	
 
 }
 
